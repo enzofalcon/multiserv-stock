adc5c87a88d721052790d2af8e0e882cd4eedaf8
Enzo Falcón <enalfaga@gmail.com>
2026-03-07 23:17:47 -0300

    Update Database.php

--- api-stock/src/Database.php
+++ api-stock/src/Database.php
@@ -1,3 +1,4 @@
+
 <?php
 
 class Database {
@@ -9,14 +10,43 @@ class Database {
         $user = getenv('DB_USER') ?: 'root';
         $pass = getenv('DB_PASS') ?: '';
 
-        return new PDO(
-            "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-            $user,
-            $pass,
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]
-        );
+        try {
+
+            return new PDO(
+                "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+                $user,
+                $pass,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]
+            );
+
+        } catch (PDOException $e) {
+            die("ERROR DB: " . $e->getMessage());
+        }
     }
 }
+
+//  <?php
+
+// class Database {
+//     public static function getConnection(): PDO
+//     {
+//         $host = getenv('DB_HOST') ?: 'localhost';
+//         $port = getenv('DB_PORT') ?: '3306';
+//         $db   = getenv('DB_NAME') ?: 'multiserv';
+//         $user = getenv('DB_USER') ?: 'root';
+//         $pass = getenv('DB_PASS') ?: '';
+
+//         return new PDO(
+//             "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+//             $user,
+//             $pass,
+//             [
+//                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//             ]
+//         );
+//     }
+// } 
