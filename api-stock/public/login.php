5f24becf42bbfca282e9a4bc92dbd68c3f14e1f0
Enzo Falcón <enalfaga@gmail.com>
2026-03-08 18:34:35 -0300

    update login docker

--- api-stock/public/login.php
+++ api-stock/public/login.php
@@ -4,26 +4,10 @@ header('Content-Type: application/json');
 
 require_once __DIR__ . "/../src/Database.php";
 
-$raw = file_get_contents("php://input");
-
-echo json_encode([
-    "raw_input" => $raw,
-    "_POST" => $_POST,
-    "_REQUEST" => $_REQUEST
-]);
-exit;
-
 $data = json_decode(file_get_contents("php://input"), true);
 
 $email = trim($data["email"] ?? "");
 $password = trim($data["password"] ?? "");
-
-echo json_encode([
-    "data_recibida" => $data,
-    "email" => $email,
-    "password" => $password
-]);
-exit;
 
 if (!$email || !$password) {
     echo json_encode(["error" => "Datos incompletos"]);
